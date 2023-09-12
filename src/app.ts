2be494b79179dc059a665e2d783be4cb6dc34174
idcristi <cristi.duluta@gmail.com>
2023-09-12 17:50:20 +0200

    chore: Remove ts-ignore comment

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
 import { getHistoricalBalances } from "./services/getHistoricalBalances";
 import { validatorHistoryBalance } from "./utils/validator";
+import { SortEnum } from "./utils/enums";
 
 const app = express();
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -14,8 +15,14 @@ app.get(
   async (req: Request, res: Response) => {
     try {
       const { from, to, sort } = req.query;
-      //@ts-ignore
-      const historicalBalances = await getHistoricalBalances(from, to, sort);
+      const fromDate = new Date(from as string);
+      const toDate = new Date(to as string);
+
+      const historicalBalances = await getHistoricalBalances(
+        fromDate,
+        toDate,
+        sort as SortEnum
+      );
       return res.json(historicalBalances);
     } catch (error) {
       return res.status(400).json({ error: (error as Error).message });
